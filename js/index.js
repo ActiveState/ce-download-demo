b3f5e28e399f9ad40358ad1412ed63380f41d666
lukep <luke.pacholski@gmail.com>
2019-03-21 14:32:57 -0700

    Python links demo

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let body = document.querySelector("body");
+
+body.addEventListener("click", (e) => {
+    let clickedLink = e.target;
+    let forkLink = clickedLink.getAttribute("forkLink");
+    let downloadLink = clickedLink.getAttribute("downloadLink");
+    
+    document.querySelector(".modal a.fork-link").setAttribute("href", forkLink);
+    document.querySelector(".modal a.download-link").setAttribute("href", downloadLink);
+
+    body.classList.toggle("show-modal");
+});
+
+document.querySelector(".modal").addEventListener("click", (e) => {
+    e.stopPropagation();
+});
